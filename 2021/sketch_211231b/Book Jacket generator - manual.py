f0d6f76ee6d43a8ac499de4ed18e773a54aa468c
Kempton Mooney <fkmooney@users.noreply.github.com>
2021-12-31 19:32:06 -0500

    Add files via upload

--- 2021/sketch_211231b/Book Jacket generator - manual.py
+++ 2021/sketch_211231b/Book Jacket generator - manual.py
@@ -3,24 +3,24 @@ from PIL import Image, ImageFont, ImageDraw, ImageOps, ImageEnhance
 ### VAIRABLES #################################################################
 theme_color = ('black')
 text_copy = """
-THE ART OF 
-WORLDLY WISDOM 
+the art of worldy wisdom
 
-Baltasar Gracian"""
+baltasar gracian"""
 
-spine_copy = "THE ART OF WORLDLY WISDOM - Baltasar Gracian"
+spine_copy = "the art of worldy wisdom   baltasar gracian"
 
 copyright_text = """COPYRIGHT 2021. 
 ALL RIGHTS RESERVED. 
-KEMPTONMOONEY.COM"""
+FKM BOOKS"""
 back_copy = """
 
 
 """
 
-FRONT_font = ImageFont.truetype('Montserrat-ExtraBold.otf',size=140) # can also use Arial
-sidefont = ImageFont.truetype('Montserrat-ExtraBold.otf',size=70)
-BACK_font = ImageFont.truetype('Montserrat-Bold.otf',size=55)
+FRONT_font = ImageFont.truetype('Montserrat-Medium.otf',size=120) # can also use Arial
+sidefont = ImageFont.truetype('Montserrat-Medium.otf',size=70)
+sidefont_lower = ImageFont.truetype('Montserrat-Bold.otf',size=50)
+BACK_font = ImageFont.truetype('Montserrat-Medium.otf',size=55)
 CRfont = ImageFont.truetype('Montserrat-Bold.otf',size=40)
 
 pages = 150
@@ -54,12 +54,12 @@ img_draw = ImageDraw.Draw(blank_image)
 #img_draw.line((dpi_total_width/2+dpi_spine_width/2, 0, dpi_total_width/2+dpi_spine_width/2, dpi_total_height), width=1, fill='blue')
 
 # Front cover text
-text = img_draw.text((dpi_total_width/2+dpi_spine_width/2+138, 150), text_copy, font=FRONT_font, fill='white')
+text = img_draw.text((dpi_total_width/2+dpi_spine_width/2+138, 190), text_copy, font=FRONT_font, fill='white')
 
 # image
 img = Image.open("design.png")
-img = img.resize((int(dpi_total_width/2-dpi_spine_width), int(dpi_total_width/2-dpi_spine_width)))
-blank_image.paste(img,(2005,dpi_total_height - int(dpi_total_width/2-dpi_spine_width)))
+img = img.resize((int(dpi_total_width/2-dpi_spine_width/2), int(dpi_total_width/2-dpi_spine_width/2)))
+blank_image.paste(img,(int(dpi_total_width/2+dpi_spine_width/2),dpi_total_height - int(dpi_total_width/2-dpi_spine_width/2)))
 
 blank_image.paste(img,(0,0))
 
@@ -80,10 +80,10 @@ blank_image.paste(w_right, (1864, 225), w_right)
 f = ImageFont.load_default()
 txt = Image.new('L', (3300, 72))  # dimensions of layer for sideways text
 d = ImageDraw.Draw(txt)
-d.text((0, 0), "FKM BOOKS", font=sidefont, fill='white')
+d.text((0, 0), "FKM BOOKS", font=sidefont_lower, fill='white')
 w_left = txt.rotate(90, expand=1)  # expand: expands the output to be large enough to hold the rotated image
 w_right = txt.rotate(-90, expand=1)
-blank_image.paste(w_right, (1864, 2250), w_right)
+blank_image.paste(w_right, (1864, 2350), w_right)
 
 # save image
 blank_image.save('Book_Jacket.jpg')
