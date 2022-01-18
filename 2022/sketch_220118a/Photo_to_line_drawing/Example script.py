02997260f4b948ea4d9bd6c773a1fadc0ff10e28
Kempton Mooney <fkmooney@users.noreply.github.com>
2022-01-18 10:21:32 -0500

    Add files via upload

---
+++ 2022/sketch_220118a/Photo_to_line_drawing/Example script.py
@@ -0,0 +1,7 @@
+import linedraw
+
+lines = linedraw.sketch("images/tulips.jpg")  # return list of polylines, eg.
+linedraw.visualize(lines)                    # simulates plotter behavior
+
+# output is to out folder as svg
+
